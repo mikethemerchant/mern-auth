169e35f2d7e8df59fd688176494f9eefcc81035b
mikethemerchant <mikethemerchant@gmail.com>
2023-06-09 15:55:08 -0400

    logout destroy cookie 11335

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,46 +6,66 @@ import User from '../models/userModel.js';
 // route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'User is authenticated' });
+    const { email, password } = req.body;
+
+    const user = await User.findOne({email: email});
+
+    if (user && (await user.matchPassword(password))) {
+      generateToken(res, user._id);
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
+    }
 });
 
-// @desc Register a new user
-// route POST /api/users
-// @access Public
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
-
-    const userExists = await User.findOne({ email: email });
-
+  
+    const userExists = await User.findOne({ email });
+  
     if (userExists) {
-        res.status(400);
-        throw new Error('User already exists');
+      res.status(400);
+      throw new Error('User already exists');
     }
-
+  
     const user = await User.create({
-        name: name,
-        email: email,
-        password: password
+      name,
+      email,
+      password,
     });
-
+  
     if (user) {
-        generateToken(res, user._id);
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email:user.email
-        });
+      generateToken(res, user._id);
+  
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
     } else {
-        res.status(400);
-        throw new Error('Invalid user data');
+      res.status(400);
+      throw new Error('Invalid user data');
     }
-});
+  });
 
 // @desc Logout user
 // route POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Logout user' });
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0),
+    });
+
+    res.status(200).json({ message: 'User logged out' });
 });
 
 // @desc Get user profile
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -30,6 +30,11 @@ const userSchema = mongoose.Schema({
     this.password = await bcrypt.hash(this.password, salt);
   });
 
+  // Match user entered password to hashed password in database
+  userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
+
 const User = mongoose.model('User', userSchema);
 
 export default User;

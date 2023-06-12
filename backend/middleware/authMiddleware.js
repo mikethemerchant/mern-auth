89bf9a98fb6a538a3ed6ef0483bde1a44d77e07f
mikethemerchant <mikethemerchant@gmail.com>
2023-06-12 11:35:40 -0400

    back end done 12811

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -10,15 +10,12 @@ const protect = asyncHandler( async (req, res, next) => {
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
             req.user = await User.findById(decoded.userId).select('-password');
-
             next();
         } catch (error) {
             res.status(401);
             throw new Error('Not authorized, no token');
         }
-
     } else {
         res.status(401);
         throw new Error('Not authorized, no token');

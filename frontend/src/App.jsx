d0751d60b66ae9f8c9a7aa9dbd14a27afcfafec9
mikethemerchant <mikethemerchant@gmail.com>
2023-06-14 08:35:01 -0400

    up to react router working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
       <Header />
-      <Container className='py-3'>
-        <Outlet />
+      <Container className='my-2'>
+        <HomeScreen />
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

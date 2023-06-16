ffc50dcbdf1d034a9ffb47ab61143bc522e05dc2
mikethemerchant <mikethemerchant@gmail.com>
2023-06-16 11:13:27 -0400

    before react toastify 237

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,8 +18,8 @@ const authUser = asyncHandler(async (req, res) => {
         email: user.email
       });
     } else {
-      res.status(400);
-      throw new Error('Invalid user data');
+      res.status(401);
+      throw new Error('Invalid username or password');
     }
 });
 
